540028badd66ecf66cbc0a693f1405b6cb0dfa9e
Yuto <sasayu8190@gmail.com>
2024-07-02 15:15:03 +0900

    feat: add 尺取り法

--- A13/13.go
+++ A13/13.go
@@ -1,27 +1,23 @@
 package main
 
 import "fmt"
-func main(){
-	n,diff :=0,0
-	fmt.Scan(&n,&diff)
-	array:=make([]int,n)
 
-	counter:=0
-	for i:=0;i<n;i++{
+func main() {
+	n, diff := 0, 0
+	fmt.Scan(&n, &diff)
+	array := make([]int, n)
+
+	counter := 0
+	for i := 0; i < n; i++ {
 		fmt.Scan(&array[i])
 	}
-	
-	for i:=1;i<n;i++{
-		left_p,right_p:=0,i-1
-		for left_p<=right_p{
-			mid := (left_p+right_p)/2
-			if array[i]-array[mid] <=diff{
-				right_p=mid-1
-			}else{
-				left_p=mid+1
-			}
+
+	j := 0
+	for i := 1; i < n; i++ {
+		for j < i && array[i]-array[j] > diff {
+			j++
 		}
-		counter+=i - left_p
+		counter += i - j
 	}
 	fmt.Println(counter)
 }
