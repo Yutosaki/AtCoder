e8c102e4bc7150e2b0e59cd808008efdaed44b14
Yuto <sasayu8190@gmail.com>
2024-07-03 12:49:06 +0900

    feat: DP

---
+++ A16/a.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main(){
+	n:=0
+	fmt.Scan(&n)
+	arrayA:=make([]int,n+1)
+	arrayB:=make([]int,n+1)
+	for i:=2;i<=n;i++{
+		fmt.Scan(&arrayA[i])
+	}
+	for i:=3;i<=n;i++{
+		fmt.Scan(&arrayB[i])
+	}
+
+	timer:=make([]int, n+1)
+	timer[2]=arrayA[2]
+	for i:=3;i<=n;i++{
+		if timer[i-2]+arrayB[i]<timer[i-1]+arrayA[i]{
+			timer[i]=timer[i-2]+arrayB[i]
+		}else{
+			timer[i]=timer[i-1]+arrayA[i]
+		}
+	}
+	
+	fmt.Println(timer[n])
+}
