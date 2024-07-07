a5050171f8ccb39c1d23730552b9d25842ea018e
Yuto <sasayu8190@gmail.com>
2024-07-07 18:28:50 +0900

    feat: 復習1。dpのboolを論理和で表す

--- A18/a.go
+++ A18/a.go
@@ -1,40 +1,34 @@
 package main
 
 import "fmt"
-func main(){
-	n,sum:=0, 0
-	fmt.Scan(&n,&sum)
-	array:=make([]int, n+1)
-	for i:=1;i<=n;i++{
-		fmt.Scan(&array[i])
+
+func main() {
+	n, s := 0, 0
+	fmt.Scan(&n, &s)
+	A := make([]int, n+1)
+	for i := 1; i <= n; i++ {
+		fmt.Scan(&A[i])
 	}
 
-	dp:=make([][]bool, n+1)
-	for i:=0;i<=n;i++{
-		dp[i]=make([]bool, sum+1)
+	dp := make([][]bool, n+1)
+	for i := 0; i <= n; i++ {
+		dp[i] = make([]bool, s+1)
 	}
 
-	dp[0][0]=true
-	for i:=1;i<=n;i++{
-		for j:=0;j<=sum;j++{
-			if j < array[i]{
-				if dp[i-1][j]==true {
-					dp[i][j]=true
-				}else{
-					dp[i][j]=false
-				}
-			}else{
-				if dp[i-1][j-array[i]]==true || dp[i-1][j]==true{
-					dp[i][j]=true
-				}else{
-					dp[i][j]=false
-				}
+	dp[0][0] = true
+	for i := 1; i <= n; i++ {
+		for j := 0; j <= s; j++ {
+			if j < A[i] {
+				dp[i][j] = dp[i-1][j]
+			} else {
+				dp[i][j] = dp[i-1][j] || dp[i-1][j-A[i]]
 			}
 		}
 	}
-	if dp[n][sum]==true{
+
+	if dp[n][s] == true {
 		fmt.Println("Yes")
-	}else{
+	} else {
 		fmt.Println("No")
 	}
 }
