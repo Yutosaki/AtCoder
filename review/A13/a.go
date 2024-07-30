f148e321fb4cff87c449617c3ffb12a11a6d11de
Yutosaki <sasayu8190@gmail.com>
2024-07-30 18:08:21 +0900

    feat: 1almost 1ok 18min 別解

--- review/A13/a.go
+++ review/A13/a.go
@@ -11,19 +11,19 @@ func main() {
 		fmt.Scan(&A[i])
 	}
 
+	maxEnd := make([]int, n)
 	counter := 0
-	for i := 0; i < n; i++ {
-		left := i + 1
-		right := n
-		for left < right {
-			mid := (left + right) / 2
-			if A[mid]-A[i] <= k {
-				left = mid + 1
-			} else {
-				right = mid
-			}
+	for i := 0; i < n-1; i++ {
+		if i == 0 {
+			maxEnd[i] = 0
+		} else {
+			maxEnd[i] = maxEnd[i-1]
 		}
-		counter += left - i - 1
+
+		for maxEnd[i] < n && A[maxEnd[i]]-A[i] <= k {
+			maxEnd[i]++
+		}
+		counter += maxEnd[i] - i - 1
 	}
 
 	fmt.Println(counter)
