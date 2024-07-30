35e1d18f24b7b26bc348aaba5298d1a7bd1468db
Yutosaki <sasayu8190@gmail.com>
2024-07-30 18:04:11 +0900

    feat: 1almost 1ok 18min

--- review/A13/a.go
+++ review/A13/a.go
@@ -1,11 +1,9 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	var n, k int
+	n, k := 0, 0
 	fmt.Scan(&n, &k)
 
 	A := make([]int, n)
@@ -17,19 +15,15 @@ func main() {
 	for i := 0; i < n; i++ {
 		left := i + 1
 		right := n
-
-		// 二分探索でA[i] + K以下の最大の位置を見つける
 		for left < right {
 			mid := (left + right) / 2
-			if A[mid] <= A[i] + k {
+			if A[mid]-A[i] <= k {
 				left = mid + 1
 			} else {
 				right = mid
 			}
 		}
-
-		// 見つけた位置leftはA[i] + K以下の最大の位置 + 1
-		counter += left - (i + 1)
+		counter += left - i - 1
 	}
 
 	fmt.Println(counter)
