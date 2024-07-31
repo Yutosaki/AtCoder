5644eee52caaf8173abf2e18b9b43b4b49daadea
Yutosaki <sasayu8190@gmail.com>
2024-07-31 11:37:24 +0900

    feat: 1ng 1almost 25min

--- review/A14/a.go
+++ review/A14/a.go
@@ -8,12 +8,10 @@ import (
 func main() {
 	n, k := 0, 0
 	fmt.Scan(&n, &k)
-
 	A := make([]int, n)
 	B := make([]int, n)
 	C := make([]int, n)
 	D := make([]int, n)
-
 	for i := 0; i < n; i++ {
 		fmt.Scan(&A[i])
 	}
@@ -28,49 +26,40 @@ func main() {
 	}
 
 	P := make([]int, n*n)
+	Q := make([]int, n*n)
 	for i := 0; i < n; i++ {
 		for j := 0; j < n; j++ {
-			P = append(P, A[i]+B[j])
+			P[i*n+j] = A[i] + B[j]
 		}
 	}
-
-	Q := make([]int, 0, n*n)
 	for i := 0; i < n; i++ {
 		for j := 0; j < n; j++ {
-			Q = append(Q, C[i]+D[j])
+			Q[i*n+j] = C[i] + D[j]
 		}
 	}
+	sort.Slice(Q, func(i, j int) bool { return Q[i] < Q[j] })
 
-	sort.Ints(Q)
-
-	found := false
-	for _, p := range P {
-		target := k - p
-		if binarySearch(Q, target) {
-			found = true
-			break
+	for i := 0; i < len(P); i++ {
+		if closest(k-P[i], Q) {
+			fmt.Println("Yes")
+			return
 		}
 	}
-
-	if found {
-		fmt.Println("Yes")
-	} else {
-		fmt.Println("No")
-	}
+	fmt.Println("No")
 }
 
-func binarySearch(array []int, x int) bool {
+func closest(target int, array []int) bool {
 	left := 0
 	right := len(array) - 1
 
 	for left <= right {
-		mid := left + (right-left)/2
-		if array[mid] == x {
-			return true
-		} else if array[mid] < x {
+		mid := (left + right) / 2
+		if array[mid] < target {
 			left = mid + 1
-		} else {
+		} else if array[mid] > target {
 			right = mid - 1
+		} else {
+			return true
 		}
 	}
 	return false
