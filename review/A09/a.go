885b18febf3b6fd8cf060679fd59e4a5a6c09f96
Yutosaki <sasayu8190@gmail.com>
2024-08-05 14:53:01 +0900

    feat: 1almost 2ok 7min

--- review/A09/a.go
+++ review/A09/a.go
@@ -6,35 +6,38 @@ func main() {
 	h, w, n := 0, 0, 0
 	fmt.Scan(&h, &w, &n)
 
-	matrix := make([][]int, h+2)
-	for i := 0; i <= h+1; i++ {
-		matrix[i] = make([]int, w+2)
+	matrix := make([][]int, h+1)
+	for i := 0; i <= h; i++ {
+		matrix[i] = make([]int, w+1)
 	}
 
 	for i := 0; i < n; i++ {
-		a, b, c, d := 0, 0, 0, 0
-		fmt.Scan(&a, &b, &c, &d)
-		matrix[a][b]++
-		matrix[a][d+1]--
-		matrix[c+1][b]--
-		matrix[c+1][d+1]++
+		A, B, C, D := 0, 0, 0, 0
+		fmt.Scan(&A, &B, &C, &D)
+		matrix[A-1][B-1]++
+		matrix[A-1][D]--
+		matrix[C][B-1]--
+		matrix[C][D]++
 	}
 
-	for i := 1; i <= h+1; i++ {
-		for j := 1; j <= w+1; j++ {
+	for i := 0; i <= h; i++ {
+		for j := 1; j <= w; j++ {
 			matrix[i][j] += matrix[i][j-1]
 		}
 	}
 
-	for j := 1; j <= w+1; j++ {
-		for i := 1; i <= h+1; i++ {
+	for j := 0; j <= w; j++ {
+		for i := 1; i <= h; i++ {
 			matrix[i][j] += matrix[i-1][j]
 		}
 	}
 
-	for i := 1; i <= h; i++ {
-		for j := 1; j <= w; j++ {
-			fmt.Print(matrix[i][j], " ")
+	for i := 0; i < h; i++ {
+		for j := 0; j < w; j++ {
+			fmt.Print(matrix[i][j])
+			if j != w {
+				fmt.Print(" ")
+			}
 		}
 		fmt.Println()
 	}
