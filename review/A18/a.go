a88e8a752fb04f0156799a087095074675ded29a
Yutosaki <sasayu8190@gmail.com>
2024-08-10 21:50:12 +0900

    feat: 1ng 1almost 15min

--- review/A18/a.go
+++ review/A18/a.go
@@ -6,28 +6,26 @@ func main() {
 	n, s := 0, 0
 	fmt.Scan(&n, &s)
 
-	A := make([]int, n)
-	for i := 0; i < n; i++ {
+	A := make([]int, n+1)
+	for i := 1; i <= n; i++ {
 		fmt.Scan(&A[i])
 	}
 
-	dp := make([][]bool, n+1)
+	matrix := make([][]bool, n+1)
 	for i := 0; i <= n; i++ {
-		dp[i] = make([]bool, s+1)
-		dp[i][0] = true
+		matrix[i] = make([]bool, s+1)
 	}
 
+	matrix[0][0] = true
 	for i := 1; i <= n; i++ {
 		for j := 0; j <= s; j++ {
-			if j >= A[i-1] {
-				dp[i][j] = dp[i-1][j] || dp[i-1][j-A[i-1]]
-			} else {
-				dp[i][j] = dp[i-1][j]
+			if matrix[i-1][j] || (j-A[i] >= 0 && matrix[i-1][j-A[i]]) {
+				matrix[i][j] = true
 			}
 		}
 	}
 
-	if dp[n][s] {
+	if matrix[n][s] {
 		fmt.Println("Yes")
 	} else {
 		fmt.Println("No")
