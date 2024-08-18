71c6407962f8094bc0b38ec6f5f39d28e2dce126
Yutosaki <sasayu8190@gmail.com>
2024-08-18 23:35:48 +0900

    feat: 2ng 20min

--- review/A19/a.go
+++ review/A19/a.go
@@ -2,37 +2,41 @@ package main
 
 import "fmt"
 
+type jewelry struct {
+	weight int
+	value  int
+}
+
 func max(i, j int) int {
-	if i > j {
-		return i
+	if i < j {
+		return j
 	}
-	return j
+	return i
 }
 
 func main() {
-	n, W := 0, 0
-	fmt.Scan(&n, &W)
-	w := make([]int, n+1)
-	v := make([]int, n+1)
+	n, w := 0, 0
+	fmt.Scan(&n, &w)
 
+	jewelrys := make([]jewelry, n+1)
 	for i := 1; i <= n; i++ {
-		fmt.Scan(&w[i], &v[i])
+		fmt.Scan(&jewelrys[i].weight, &jewelrys[i].value)
 	}
 
-	dp := make([][]int, n+1)
+	dp := make([][]int, n+1) //i個の宝石で重さjを越えない様にする
 	for i := 0; i <= n; i++ {
-		dp[i] = make([]int, W+1)
+		dp[i] = make([]int, w+1)
 	}
 
 	for i := 1; i <= n; i++ {
-		for j := 1; j <= W; j++ {
-			if j < w[i] {
+		for j := 1; j <= w; j++ {
+			if j < jewelrys[i].weight {
 				dp[i][j] = dp[i-1][j]
 			} else {
-				dp[i][j] = max(dp[i-1][j], dp[i-1][j-w[i]]+v[i])
+				dp[i][j] = max(dp[i-1][j], dp[i-1][j-jewelrys[i].weight]+jewelrys[i].value)
 			}
 		}
 	}
 
-	fmt.Println(dp[n][W])
+	fmt.Println(dp[n][w])
 }
