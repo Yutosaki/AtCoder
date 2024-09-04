81aa3ae7acbbf464e0a3d2e3160d68a77639d708
Yutosaki <sasayu8190@gmail.com>
2024-09-04 23:26:43 +0900

    feat: 1ng 1ok 10min

--- review/A20/a.go
+++ review/A20/a.go
@@ -1,41 +1,32 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
+import "fmt"
 
-func max(i, j int) int {
-	if i > j {
-		return i
+func max(a, b int) int {
+	if a>b {
+		return a
 	}
-	return j
+	return b
 }
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	S := scanner.Text()
-	scanner.Scan()
-	T := scanner.Text()
+	s,t:="", ""
+	fmt.Scan(&s, &t)
 
-	n := len(S)
-	m := len(T)
-
-	dp := make([][]int, n+1)
-	for i := 0; i <= n; i++ {
-		dp[i] = make([]int, m+1)
+	dp:=make([][]int, len(s)+1)
+	for i:=0; i<len(s)+1; i++ {
+		dp[i]=make([]int, len(t)+1)
 	}
 
-	for i := 1; i <= n; i++ {
-		for j := 1; j <= m; j++ {
-			if S[i-1] == T[j-1] {
-				dp[i][j] = max(max(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]+1)
+	for i:=1; i<=len(s); i++ {
+		for j:=1; j<=len(t); j++ {
+			if s[i-1]==t[j-1] {
+				dp[i][j]=dp[i-1][j-1]+1
 			} else {
-				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+				dp[i][j]=max(dp[i-1][j], dp[i][j-1])
 			}
 		}
 	}
-	fmt.Println(dp[n][m])
+
+	fmt.Println(dp[len(s)][len(t)])
 }
