4449094fb6198f68366f2ee5550de9204e2580f4
Yutosaki <sasayu8190@gmail.com>
2024-09-07 23:53:37 +0900

    feat: 1ng 1ok 2almost 20min

--- review/A17/a.go
+++ review/A17/a.go
@@ -5,42 +5,45 @@ import "fmt"
 func main() {
 	n := 0
 	fmt.Scan(&n)
-
 	A := make([]int, n+1)
 	B := make([]int, n+1)
 
 	for i := 2; i <= n; i++ {
 		fmt.Scan(&A[i])
 	}
-
 	for i := 3; i <= n; i++ {
 		fmt.Scan(&B[i])
 	}
 
-	root := make([]int, n+1)
-	root[2] = A[2]
+	dp := make([]int, n+1)
+	dp[2] = A[2]
 	for i := 3; i <= n; i++ {
-		if root[i-1]+A[i] < root[i-2]+B[i] {
-			root[i] = root[i-1] + A[i]
+		if dp[i-1]+A[i] < dp[i-2]+B[i] {
+			dp[i] = dp[i-1] + A[i]
 		} else {
-			root[i] = root[i-2] + B[i]
+			dp[i] = dp[i-2] + B[i]
 		}
 	}
 
-	revroot := make([]int, 0)
-	for i := n; i >= 1; i-- {
-		if root[i] == root[i-1]+A[i] {
-			revroot = append(revroot, i-1)
-		} else if root[i] == root[i-2]+B[i] {
-			revroot = append(revroot, i-2)
+	revRoot := make([]int, 0)
+	revRoot = append(revRoot, n)
+
+	for i := n; i >= 1; {
+		if dp[i] == dp[i-1]+A[i] {
+			revRoot = append(revRoot, i-1)
 			i--
+		} else {
+			revRoot = append(revRoot, i-2)
+			i -= 2
 		}
 	}
+	fmt.Println(len(revRoot) - 1)
 
-	fmt.Println(len(revroot))
-
-	for i := len(revroot) - 2; i >= 0; i-- {
-		fmt.Printf("%d ", revroot[i])
+	for i := len(revRoot) - 2; i >= 0; i-- {
+		fmt.Print(revRoot[i])
+		if i != 0 {
+			fmt.Print(" ")
+		}
 	}
-	fmt.Println(n)
+	fmt.Println()
 }
