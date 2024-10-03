eb9d58cdb195ca48ee7b57829e1dfabe860c7c29
Yutosaki <sasayu8190@gmail.com>
2024-10-03 22:54:34 +0900

    faet: 2ng 20min

--- review/A22/a.go
+++ review/A22/a.go
@@ -1,41 +1,38 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
+
+func main() {
+	n := 0
+	fmt.Scan(&n)
+
+	A := make([]int, n)
+	B := make([]int, n)
+
+	for i := 1; i < n; i++ {
+		fmt.Scan(&A[i])
+	}
+	for i := 1; i < n; i++ {
+		fmt.Scan(&B[i])
+	}
+
+	dp := make([]int, n+1)
+	for i:=0;i<n;i++{
+		dp[i]=-2147483648
+	}
+
+	dp[1]=0
+	for i := 1; i < n; i++ {
+		dp[A[i]] = max(dp[A[i]], dp[i]+100)
+		dp[B[i]] = max(dp[B[i]], dp[i]+150)
+	}
+
+	fmt.Println(dp[n])
+}
 
 func max(i, j int) int {
 	if i > j {
 		return i
 	}
 	return j
-}
-
-func main() {
-	var N int
-	fmt.Scan(&N)
-
-	A := make([]int, N)
-	B := make([]int, N)
-	for i := 1; i < N; i++ {
-		fmt.Scan(&A[i])
-	}
-	for i := 1; i < N; i++ {
-		fmt.Scan(&B[i])
-	}
-
-	dp := make([]int, N+1)
-	
-	dp[1] = 0
-	for i := 2; i <= N; i++ {
-		dp[i] = math.MinInt32
-	}
-
-	for i := 1; i < N; i++ {
-		dp[A[i]] = max(dp[A[i]], dp[i]+100)
-		dp[B[i]] = max(dp[B[i]], dp[i]+150)
-	}
-
-	fmt.Println(dp[N])
 }
